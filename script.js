11ef00f7390a7c6732227bbe7cdd69659e46f9f6
Ash Mirza <arshaanwork123@gmail.com>
2023-08-23 21:57:46 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
-// script.js
 const navLinks = document.querySelectorAll('nav a');
 const sections = document.querySelectorAll('section');
+const introText = document.querySelector('.intro-text');
 
 navLinks.forEach((link, index) => {
-    link.addEventListener('click', () => {
-        sections.forEach((section) => {
-            section.style.display = 'none';
-        });
-        sections[index].style.display = 'block';
+  link.addEventListener('click', () => {
+    // Hide the intro text
+    introText.style.display = 'none';
+
+    // Show the clicked section's content and hide other sections
+    sections.forEach((section, sectionIndex) => {
+      if (sectionIndex === index) {
+        section.classList.add('active');
+      } else {
+        section.classList.remove('active');
+      }
     });
+  });
 });
